e56fdaa03f3deb7a3532b38b56968206c069a120
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-02-02 01:39:10 -0500

    A2 2.1 complete

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,35 @@
+export default function Float() {
+    return (
+        <div id="wd-float-divs">
+            <h2>Float</h2>
+            <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus pharetra libero, id dictum est pellentesque sed. Quisque mollis enim quis imperdiet dapibus. Curabitur ornare sollicitudin accumsan. Vivamus at lobortis ante. Morbi sit amet odio facilisis, pulvinar tellus non, tempor diam. Nullam non porttitor turpis. Nam ut ullamcorper urna. Nullam mi risus, malesuada et luctus sed, eleifend vel nibh. Fusce sit amet tempor dolor.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Quisque aliquet nec mi id congue. Nullam gravida scelerisque ipsum ut tincidunt. Maecenas ultrices ultricies neque id mattis. Proin fermentum, augue id feugiat maximus, massa sapien fermentum velit, id interdum risus sapien vel mi. Fusce bibendum magna ligula, at egestas elit aliquam eget. Nam tincidunt pharetra velit, sed efficitur sem egestas sed. Fusce nec vulputate magna. Donec tristique neque lacinia egestas molestie. Ut vel tempor purus. Vivamus a purus quam. In lobortis nibh urna, eget blandit sapien mattis et. Proin vulputate, odio vitae egestas tincidunt, magna arcu cursus enim, id pulvinar lectus est luctus ipsum. Morbi tempor dignissim tellus eu finibus. Donec et libero viverra, tristique odio quis, tristique augue. Sed dignissim ultricies elit a rhoncus.
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Pellentesque at lectus quis nisl efficitur eleifend. Nulla eleifend in est vel aliquam. Cras bibendum mi purus, ut vestibulum metus accumsan eget. Fusce rutrum mi at justo lobortis mollis. Pellentesque tortor urna, finibus in vestibulum eu, imperdiet at neque. Aliquam erat volutpat. Donec ligula leo, molestie a diam eget, semper venenatis metus. Fusce non imperdiet quam. Sed convallis lorem non tincidunt lobortis. Aliquam erat volutpat. Mauris scelerisque, urna gravida feugiat consectetur, nulla turpis dignissim odio, a luctus nunc quam ut nunc.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Aenean pulvinar suscipit augue, ut mollis risus convallis eu. Mauris eget libero blandit, cursus massa sit amet, vehicula augue. Aliquam vulputate leo eget tristique fermentum. Phasellus in venenatis risus. In eget luctus arcu. Phasellus aliquam, odio vehicula interdum ornare, sem diam semper magna, sed faucibus urna libero a massa. Morbi mattis dictum eros id aliquam.
+                <div className="wd-float-done"></div>
+            </div>
+            <div>
+                <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                    Yellow 
+                </div>
+                <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                    Blue 
+                </div>
+                <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                    Red 
+                </div>
+                <img className="wd-float-right" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                <div className="wd-float-done"></div>
+            </div>
+        </div>
+    );
+}
