71ba6310a6aca13c8b630c2dbbb89e16ff4aa65d
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-02-16 17:48:34 -0500

    3.5 Completed

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,4 +1,6 @@
+import Add from "./Add";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import AddPathParameters from "./AddPathParameters";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
@@ -12,14 +14,17 @@ import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
 import FunctionDestructing from "./FunctionDestructing";
+import Highlight from "./Highlight";
 import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
 import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
 import SimpleArrays from "./SimpleArrays";
 import Spreading from "./Spreading";
+import Square from "./Square";
 import Styles from "./Styles";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
@@ -29,67 +34,80 @@ import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
+  return (
+    <div>
+      <h2>Lab 3</h2>
 
-        <VariablesAndConstants />
+      <VariablesAndConstants />
 
-        <VariableTypes />
+      <VariableTypes />
 
-        <BooleanVariables />
+      <BooleanVariables />
 
-        <IfElse />
+      <IfElse />
 
-        <TernaryOperator />
+      <TernaryOperator />
 
-        <ConditionalOutputIfElse />
+      <ConditionalOutputIfElse />
 
-        <ConditionalOutputInline />
+      <ConditionalOutputInline />
 
-        <LegacyFunctions />
+      <LegacyFunctions />
 
-        <ArrowFunctions />
+      <ArrowFunctions />
 
-        <ImpliedReturn />
+      <ImpliedReturn />
 
-        <TemplateLiterals />
+      <TemplateLiterals />
 
-        <SimpleArrays />
+      <SimpleArrays />
 
-        <ArrayIndexAndLength />
+      <ArrayIndexAndLength />
 
-        <AddingAndRemovingToFromArrays />
+      <AddingAndRemovingToFromArrays />
 
-        <ForLoops />
+      <ForLoops />
 
-        <MapFunction />
+      <MapFunction />
 
-        <FindFunction />
+      <FindFunction />
 
-        <FindIndex />
+      <FindIndex />
 
-        <FilterFunction />
+      <FilterFunction />
 
-        <JsonStringify />
+      <JsonStringify />
 
-        <House />
+      <House />
 
-        <TodoItem />
+      <TodoItem />
 
-        <TodoList />
+      <TodoList />
 
-        <Spreading />
+      <Spreading />
 
-        <Destructing />
+      <Destructing />
 
-        <FunctionDestructing />
+      <FunctionDestructing />
 
-        <DestructingImports />
+      <DestructingImports />
 
-        <Classes />
+      <Classes />
 
-        <Styles />
-      </div>
-  );}
-  
+      <Styles />
+
+      <Add a={3} b={4} />
+
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+
+      <PathParameters />
+    </div>
+  );
+}
