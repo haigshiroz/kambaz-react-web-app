e803b038b5754b159afb1aea68b3309293b32f11
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-02-18 17:46:04 -0500

    Added link support for accounts

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,17 +1,18 @@
-import { Link } from "react-router-dom";
+import { ListGroup } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
+  const links = ["Signin", "Signup", "Profile"];
+  const { pathname } = useLocation();
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to={`/Kambaz/Account/Signin`} className="list-group-item active border border-0">
-        Signin
-      </Link>
-      <Link to={`/Kambaz/Account/Signup`} className="list-group-item text-danger border border-0">
-        Signup
-      </Link>
-      <Link to={`/Kambaz/Account/Profile`} className="list-group-item text-danger border border-0">
-        Profile
-      </Link>
+      {links.map((link) => (
+        <ListGroup.Item key={link} as={Link} to={`/Kambaz/Account/${link}`}
+          className={`className="list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>
+          {link}
+        </ListGroup.Item>
+      ))}
     </div>
   );
 }
