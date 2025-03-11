a5fd9fa3909b2cb0ef4e336b75d85d25021576c9
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-03-11 11:27:55 -0400

    4.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,14 @@
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariable";
 import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
     function sayHello() {
@@ -19,6 +26,20 @@ export default function Lab4() {
             <PassingFunctions theFunction={sayHello} />
 
             <EventObject />
+
+            <Counter />
+
+            <BooleanStateVariables />
+
+            <StringStateVariables />
+
+            <DateStateVariable />
+
+            <ObjectStateVariable />
+
+            <ArrayStateVariable />
+
+            <ParentStateComponent />
         </div>
     );
 }
